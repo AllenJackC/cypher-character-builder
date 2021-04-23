91a5bd146217bee9be1ba4479ac6df3081d2c6ca
AllenJackC <51028531+AllenJackC@users.noreply.github.com>
2021-04-23 10:03:10 -0400

    Fixed spells

--- script.js
+++ script.js
@@ -2132,7 +2132,7 @@ $(function() {
 	Airtable = require('airtable');
 	base = new Airtable({apiKey: 'keymlfH0gK5O3u0wp'}).base('appP3SrsrqcRFnoX7');
 	//Initial variables
-	curArc = 3;
+	curArc = 1;
 	curTier = 1;
 	tierDifference = 2;
 	curXP = 0;
