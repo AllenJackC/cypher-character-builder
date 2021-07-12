81f5159cfd153a943bb8b6c73014d81a24197b1f
AllenJackC <51028531+AllenJackC@users.noreply.github.com>
2021-07-12 16:59:39 -0400

    Descriptor and foci fixes

--- script.js
+++ script.js
@@ -2143,7 +2143,7 @@ $(function() {
 	Airtable = require('airtable');
 	base = new Airtable({apiKey: 'keymlfH0gK5O3u0wp'}).base('appP3SrsrqcRFnoX7');
 	//Initial variables
-	curArc = 1;
+	curArc = 3;
 	curTier = 1;
 	tierDifference = 2;
 	curXP = 0;
